62614da3268fb25a672433ef8f3c4f172b9b9ec6
Angel Sinn <angelkysinn@gmail.com>
2021-02-01 22:56:39 +0000

    Implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// Function implementation
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(1, 1.1);
