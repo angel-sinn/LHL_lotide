c2540ac0f09ecb736534aafd36cd111cbf3747a9
Angel Sinn <angelkysinn@gmail.com>
2021-02-03 01:57:19 +0000

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+console.log(assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]));
+console.log(assertArraysEqual([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]));
+console.log(assertArraysEqual([2, 4], [1, 3, 5]));
