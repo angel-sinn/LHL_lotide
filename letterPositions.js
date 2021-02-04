9fdf961bc3238ee9b3409b67b3a58e615ba25660
Angel Sinn <angelkysinn@gmail.com>
2021-02-04 01:12:28 +0000

    Implement letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,48 @@
+// Test assertion functions
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+// Actual function
+
+const letterPositions = function (sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// Test cases
+
+console.log(letterPositions("hello there"));
+console.log(letterPositions("lighthouse labs"));
+
+const result1 = letterPositions("hello there");
+const result2 = letterPositions("lighthouse labs");
+
+assertArraysEqual(result1["h"], [0, 7]);
+assertArraysEqual(result2["s"], [8, 14]);
