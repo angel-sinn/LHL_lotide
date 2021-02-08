bdc2fe64102e87ce0555a09437be86d759478a2e
Angel Sinn <angelkysinn@gmail.com>
2021-02-08 22:04:03 +0000

    Refactor with exports and require

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -18,6 +8,4 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-console.log(assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]));
-console.log(assertArraysEqual([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]));
-console.log(assertArraysEqual([2, 4], [1, 3, 5]));
+module.exports = assertArraysEqual;
